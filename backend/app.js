5d548d9232bae0a5142cded9fc1d9660d1c270f2
sohel khalifa <sohelkhalifa99@gmail.com>
2020-01-12 21:30:48 +0530

    Allowed all cors

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,12 @@
 import express from 'express';
-
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
 import indexRouter from './routes/index';
 
 const app = express();
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
