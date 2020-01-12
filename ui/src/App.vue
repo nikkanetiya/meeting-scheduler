ff17873770f03ccbc3f38e0ced4340804ccd3470
sohel khalifa <sohelkhalifa99@gmail.com>
2020-01-12 21:35:32 +0530

    Ant css and margins added

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -9,16 +9,16 @@
   </div>
 </template>
 
-
-
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
+  margin-left: 20%;
+  margin-right: 20%;
 }
 
 #nav {
