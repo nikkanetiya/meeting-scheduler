3187d4cffcdffb2ea37494fae6f6d974e0f5e1cb
sohel khalifa <sohelkhalifa99@gmail.com>
2020-01-23 23:11:09 +0530

    API: Added some utility functions

--- backend/libs/utils.js
+++ backend/libs/utils.js
@@ -13,9 +13,15 @@ const transformObj = obj => {
     obj.startTime = moment(new Date(obj.startTime._seconds * 1000)).format();
   if (obj.endTime && obj.endTime instanceof Firestore.Timestamp)
     obj.endTime = moment(new Date(obj.endTime._seconds * 1000)).format();
-  if (obj.time && obj.time instanceof Firestore.Timestamp)
-    obj.time = moment(new Date(obj.time._seconds * 1000)).format();
-  return obj;
+
+  // return new object to ensure key ordering
+  return {
+    id: obj.id,
+    startTime: obj.startTime,
+    endTime: obj.endTime,
+    duration: obj.duration,
+    minutes: obj.minutes
+  };
 };
 
 const transform = data => {
@@ -25,7 +31,43 @@ const transform = data => {
   return transformObj(data);
 };
 
+const getMinutesArray = (start, end) => {
+  let minutes = [],
+    n = start;
+  while (n <= end) {
+    minutes.push(n);
+    n++;
+  }
+  return minutes;
+};
+
+const toChunks = (arr, chunkSize = 10) => {
+  var result = [];
+  for (var i = 0, len = arr.length; i < len; i += chunkSize)
+    result.push(arr.slice(i, i + chunkSize));
+  return result;
+};
+
+const createEventPayload = data => {
+  const startTime = new Date(data.startTime);
+  startTime.setSeconds(0);
+  const endTime = new Date(startTime.getTime() + data.duration * 60 * 1000 - 1);
+
+  delete data.duration;
+  const startMinutesSinceMidnight =
+    startTime.getHours() * 60 + startTime.getMinutes();
+  const endMinutesSinceMidnight =
+    endTime.getHours() * 60 + endTime.getMinutes();
+
+  minutes = getMinutesArray(startMinutesSinceMidnight, endMinutesSinceMidnight);
+
+  return { startTime, endTime, minutes };
+};
+
 module.exports = {
   jsonResponse,
-  transform
+  transform,
+  getMinutesArray,
+  toChunks,
+  createEventPayload
 };
